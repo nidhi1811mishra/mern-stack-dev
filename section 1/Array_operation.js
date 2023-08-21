3c9e860eb5f7aab42f3cc9f9cd78098ae2b7ffd8
nidhi1811mishra <nidhim1810@gamil.com>
2023-08-21 23:09:26 +0530

    section 1

--- section 1/Array_operation.js
+++ section 1/Array_operation.js
@@ -45,8 +45,27 @@ console.log();
 
 const prices = [120, 4500 , 700, 1200, 999, 2300, 640, 140]
 const newprice= prices.filter((i)=>{return i>1000 && i<3000} );
+console.log('newprice')
 console.log(newprice)
+ console.log('newprice_1')
+const newprice1 = prices.filter(p=> p>1000 && p<3000 ) 
+console.log(newprice1)
 
 console.log();
 
-console.log(parseInt(`$345.89`.slice(1)))
+const cell=[120,55, 77,85,96,75];
+const newcell=cell.filter((c) => {return c%2==0} )
+console.log(newcell );
+
+const color=['red','green','pink','oreng','black','white'];
+const newcolor = color.filter((co)=>{return co.length>4})
+console.log(newcolor)
+
+const result = nums.map((n)=>{return n**2});
+console.log(result);
+
+const price2 = ['$654.567','$6546.545','$7874.51'];
+console.log('$6545.5454'.slice(1));
+const doller = price2.map((pri)=>{return parseInt(pri.slice(1))});
+console.log('doller')
+console.log(doller)
--- section 1/object.js
+++ section 1/object.js
@@ -11,7 +11,7 @@ console.log(student.email);
 console.log(student['password'])
 student.password = 'bhfuhtwh' //replacing place
 
-student.address = 'lucknow';
+student.address = 'lucknow'; // adding new key value
 console.log(student);
 
 const smartphone = {
@@ -23,12 +23,43 @@ const smartphone = {
 console.log(smartphone.color[1]);
 smartphone.color.push("red")
 console.log(smartphone)
-smartphone.color[0] = 'skyblue';
+smartphone.color[0] = 'skyblue'; //replacing new colo
+console.log(smartphone)
+console.log()
+smartphone.color[2]='orang'
 console.log(smartphone)
 
-smartphone.color.unshift('skyblue')
+smartphone.color.unshift('pink') //adding new color at biginnig
 console.log(smartphone)
-// console.log(smartphone[0].model)
-// smartphone[2].color.push('white');
-// console.log(smartphone)
+console.log()
+const smartphones = [ 
+    {brand : 'samsang', model : 'galaxy M31', price : 49999 , color :['blue','black'] },
+    {brand : 'oppo', model : 'A15', price : 24999, color : ['silver','red'] },
+    {brand : 'Oneplse', model : 'Nord', price : 35999 , color :['black','sopparia'] },
+    {brand : 'Nokia', model : 'Lunina 345', price : 36999 , color :['white','blue'] },
+
+]
+ console.log(smartphones)
+
+ console.log(smartphones[0].model);
+ console.log(smartphones[0].color[1]);
+ console.log()
+ 
+ //print oneplse price
+
+ console.log(smartphones[2].price)
+//chnage the model of oppo
+
+smartphones[1].model='C15';
+// chnage the second color of Nokia
+smartphones[3].color[1]='green';
+
+// add a color in onepluse nord 
+smartphones[2].color.push('white');
+console.log('print new data')
+
+console.log(smartphones);
+
+ const brands = smartphones.map ((sm) => {return sm.brand});
+ console.log(brands);
 
