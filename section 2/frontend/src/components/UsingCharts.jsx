6bda00d387de44051caccc3d94f3b6c20c2a30d2
nidhi1811mishra <nidhim1810@gamil.com>
2023-10-06 16:31:34 +0530

    backend

--- section 2/frontend/src/components/UsingCharts.jsx
+++ section 2/frontend/src/components/UsingCharts.jsx
@@ -1,8 +1,8 @@
-import { Chart, CategoryScale, LinearScale, BarElement, Title,Tooltip, Legend, PointElement,LineElement } from 'chart.js';
-import React from 'react'
-import { Bar, Line } from 'react-chartjs-2';
+import { Chart, CategoryScale, LinearScale,BarElement, Title,Tooltip, Legend, PointElement,LineElement,ArcElement, } from 'chart.js';
+import React, { useRef } from 'react'
+import { Bar, Doughnut, Line,Chart as MyChart, PolarArea } from 'react-chartjs-2';
 
-Chart.register(CategoryScale, LinearScale, BarElement, Title,Tooltip, Legend, PointElement,LineElement);
+Chart.register(CategoryScale,  LinearScale, BarElement, Title,Tooltip, Legend, PointElement,LineElement,ArcElement);
 
 const UsingCharts = () => {
 
@@ -10,19 +10,35 @@ const data = {
     labels:['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
     datasets:[{
         label: 'No. of Orders',
-        data: [1000, 2000,3000,4000,5000,6000,6500,5500,4500,3500,2500,1500],
-        backgroundColor: 'blue',
+        data: [10, 20,30,40,50,60,65,55,45,38,28,15],
+        backgroundColor: ['blue',],
         borderColor:'blue'
     },
     {
         label: 'No. of Visits',
-        data: [1500, 2500,3500,4500,5500,6500,6000,5000,4000,3000,2000,1000],
+        data: [15, 25,35,45,55,65,60,50,40,30,20,10],
         backgroundColor: "purple",
         borderColor:'purple'
     }
 ]
 }
-
+const data2={
+  labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+  datasets:[{
+    label: 'No. of Orders',
+    data: [10, 20,30,40,50,60,65,55,45,38,28,15],
+    backgroundColor: ['blue','green','orange','pink','red','purple'],
+    borderColor:['blue','green','orange','pink','red','purple' ]
+  }]
+}
+//  const chartRef= useRef(null);
+// const createGradient=(ctx,chartArea)=>{
+//   const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
+//   gradient.addColorStop(0,'yellow');
+//   gradient.addColorStop(0.5,'orange' );
+//   gradient.addColorStop(1,'pink' );
+//   return gradient
+// }
 
 
   return (
@@ -38,10 +54,17 @@ const data = {
         <h2>Line Chart</h2>
         <hr />
         <Line data={data}/>
+         
+        <div style={{height:'300px'}}>
 
-        <h2>cChart</h2>
-
+        <h2>Doughnut Chart</h2>
+        <hr />
+        <Doughnut width={'100%'} data={data2} options={{ maintainAspectRatio:false}}/>
         </div>
+           {/* <MyChart data={data} type='line' ref={chartRef}/> */}
+              
+        </div>
+       
     </div>
   )
 }
